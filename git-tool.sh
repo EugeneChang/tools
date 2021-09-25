a798cc37f09f138e64f974eed6c8873730709dcd
EugeneChang <6447186+EugeneChang@users.noreply.github.com>
2021-09-24 18:37:40 -0700

    Add git-tool

---
+++ git-tool.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+git config --global alias.lg "log -20 --all --oneline --decorate --graph --pretty=format:'%C(yellow)%h|%Cred%ad|%Cblue%an|%Cgreen%d %Creset%s'"
+git config --global alias.lgs "log --all --oneline --decorate --graph --pretty=format:'%C(yellow)%h|%Cred%ad|%Cblue%an|%Cgreen%d %Creset%s'"
